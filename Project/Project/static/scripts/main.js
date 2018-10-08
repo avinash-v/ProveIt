89beca85fe9730cafb5f34f25bdf633938790145
ASMITA RAO <asmitarao777@gmail.com>
2018-10-08 15:50:29 +0530

    script files

---
+++ Project/Project/static/scripts/main.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+    //to show the login and signup button when clicked  
+    $(".menu-bar").on('click', function(e){
+        e.preventDefault();
+        $("nav").toggleClass('hide');
+        
+      
+    });
+     
+    
+
+
+
+
+
+    
+    
+
+
+
+    
+        
+   
+
+     
+ });
