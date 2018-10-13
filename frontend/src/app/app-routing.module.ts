5227d7451c799a47db6cfa644c87a4c03d43aa33
arunj18 <arunjoh@gmail.com>
2018-10-13 09:50:36 +0530

    fix angular

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,43 +1,57 @@
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 import {
-    NbAuthComponent,
-    NbLoginComponent,
-    NbRegisterComponent,
-    NbLogoutComponent,
-    NbRequestPasswordComponent,
-    NbResetPasswordComponent,
-  } from '@nebular/auth';
+  NbAuthComponent,
+  NbLoginComponent,
+  NbLogoutComponent,
+  NbRegisterComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
 
-  export const routes: Routes = [
-    {
-      path: 'auth',
-      component: NbAuthComponent,
-      children: [
-        {
-          path: '',
-          component: NbLoginComponent,
-        },
-        {
-          path: 'login',
-          component: NbLoginComponent,
-        },
-        {
-          path: 'register',
-          component: NbRegisterComponent,
-        },
-        {
-          path: 'logout',
-          component: NbLogoutComponent,
-        },
-        {
-          path: 'request-password',
-          component: NbRequestPasswordComponent,
-        },
-        {
-          path: 'reset-password',
-          component: NbResetPasswordComponent,
-        },
-      ],
-    },
-  ];
+const routes: Routes = [
+  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
+  },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages' },
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+};
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
