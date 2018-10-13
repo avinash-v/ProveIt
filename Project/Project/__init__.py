1edffe7f55994703d00773a567d9be37b9884e61
MayankKejriwal97 <mayankkejriwal97@gmail.com>
2018-10-13 19:36:14 +0530

    Made everything into APIs.Added check for Password Strength.Since the templates and static are no longer required in backend i have not included it.Updated the databse model to icnorporate users and groups using single tabkle inheritance model. Some of the paclages reuqired would be: flask_restful,validate_email,passwordmeter

--- Project/Project/__init__.py
+++ Project/Project/__init__.py
@@ -7,12 +7,13 @@ from Project.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+from flask_restful import Api
 app = Flask(__name__)
+api = Api(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # for @login_required pages using flask_login
 
-import Project.views, Project.models
+import Project.views, Project.models , Project.resources.endpoints
--- Project/Project/models.py
+++ Project/Project/models.py
@@ -1,4 +1,5 @@
-from Project import db
+from Project import db # It should be from Project import db
+from sqlalchemy.schema import PrimaryKeyConstraint
 from flask_login import UserMixin
 from Project import login
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -20,4 +21,101 @@ class User(UserMixin,db.Model):
 
 @login.user_loader
 def load_user(id):
-    return User.query.get(int(id))
+    try:
+        return User.query.get(int(id))
+    except:
+        return None
+
+
+
+#Admin has been made as a separate model not inherited because it does not have bio,intersts and all....
+
+class UserProfile(db.Model):
+    id = db.Column(db.Integer,db.ForeignKey("user.id"))
+    #Thsi will take care of usn for Students,faculty ids ...
+    collegeId = db.Column(db.Integer,index=True,unique=True,primary_key= True)
+    firstName = db.Column(db.String(128),index=True)
+    lastName = db.Column(db.String(128),index=True)
+    #DOB
+    #Assuming only one contact.If there are more than 1 contacts we need to denormalize it and shit...
+    contact = db.Column(db.String(128),index=True,unique=True)
+    bio = db.Column(db.String(512),index=True)
+    interests = db.Column(db.String(512),index=True)
+    #skill 
+    userType = db.Column(db.String(128),index=True)
+
+
+    __mapper_args__ = {
+    'polymorphic_on' : userType
+    }
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class CollegeRepresentative(UserProfile):
+    designation = db.Column(db.String(512),index=True)
+
+
+    __mapper_args__ = {
+    "polymorphic_identity" : "collegeRepresentative"
+    }
+
+class Student(UserProfile):
+    cgpa = db.Column(db.Integer,index=True,unique=True)
+
+    __mapper_args__ = {
+    "polymorphic_identity" : "student"
+    }
+
+class Faculty(UserProfile):
+    researchBody = db.Column(db.String(256),index=True)
+
+    __mapper_args__ = {
+    "polymorphic_identity" : "faculty"
+    }
+
+#A separate class for Admin...Do we really need it??
+#If yes we will add it ....
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer,index=True,unique=True,primary_key = True)
+    name = db.Column(db.String,index=True,unique=True)
+    domain = db.Column(db.String,index=True)
+    #WE should check for a referential integrity constarint over here
+    owner = db.Column(db.String,index=True)
+    subOwner = db.Column(db.String,index=True)
+    abstract = db.Column(db.String(1024),index=True)
+    groupType = db.Column(db.String,index=True)
+
+    __mapper_args__= {
+    "polymorphic_on" : groupType,
+    }
+
+class ResearchGroup(Group):
+    __mapper_args__={
+    "polymorphic_identity" : "research group"
+    }
+
+class ProjectGroup(Group):
+    researchGroup = db.Column(db.Integer,db.ForeignKey("group.id"))
+    topic = db.Column(db.String(256),index=True)
+
+    __mapper_args__={
+    "polymorphic_identity" : "project group"
+    }
+
+class UserToGroup(db.Model):
+    userId = db.Column(db.Integer,db.ForeignKey("user_profile.id"))
+    groupId = db.Column(db.Integer,db.ForeignKey("group.id"))
+    #role
+    __table_args__ = (PrimaryKeyConstraint(userId,groupId),)
+
+
+#TODO:
+#   ~ functions to map project groups with User Query.
+#   ~ Functions to map projects groups in Research group,   
+
+
+
