4b83979a49ddfc913c64725f68414f82db07388f
arunj18 <arunjoh@gmail.com>
2018-10-13 14:49:24 +0530

    added login routes

--- frontend/src/app/main/main-routing.module.ts
+++ frontend/src/app/main/main-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { NbLayoutModule, NbSidebarModule, NbSidebarService } from '@nebular/theme';
 
 const routes: Routes = [
   {
     path : '' , component : MainLayoutComponent
   }
-
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), NbLayoutModule, NbSidebarModule
+  ],
+  providers : [ NbSidebarService ],
   exports: [RouterModule]
 })
 export class MainRoutingModule { }
--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -4,11 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 
+import { NbLayoutModule, NbSidebarModule, NbSidebarService } from '@nebular/theme';
+
 @NgModule({
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    NbLayoutModule,
+    NbSidebarModule
   ],
+  providers: [ NbSidebarService ],
   declarations: [MainLayoutComponent]
 })
 export class MainModule { }
