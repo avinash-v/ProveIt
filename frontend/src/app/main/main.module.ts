9bb5256e2f5d7e51107aafdaabddce82943cc3e7
arunj18 <arunjoh@gmail.com>
2018-10-13 10:26:05 +0530

    revert to basic

---
+++ frontend/src/app/main/main.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainRoutingModule } from './main-routing.module';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MainRoutingModule
+  ],
+  declarations: [MainLayoutComponent]
+})
+export class MainModule { }
