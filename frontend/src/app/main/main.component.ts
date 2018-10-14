316b9ab66c70e8d95c6b5b2d588284f83eb26fb9
arunj18 <arunjoh@gmail.com>
2018-10-14 11:09:53 +0530

    much more better page layout

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { NbSidebarService, NbMenuService } from '@nebular/theme';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  changeDetection : ChangeDetectionStrategy.OnPush,
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  items = [{title : 'Profile' }, {title : 'Logout'}];
+  constructor(private sidebarService: NbSidebarService, private menuService: NbMenuService) { }
 
-  constructor() { }
-
+  toggleSidebar() {
+    this.sidebarService.toggle(false);
+    return false;
+  }
   ngOnInit() {
   }
 
--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { MainRoutingModule } from './main-routing.module';
 
-import { NbLayoutModule, NbSidebarModule, NbSidebarService } from '@nebular/theme';
+// tslint:disable-next-line:max-line-length
+import { NbLayoutModule, NbSidebarModule, NbSidebarService, NbActionsModule, NbUserModule, NbCardModule, NbContextMenuModule, NbMenuService, NbMenuModule } from '@nebular/theme';
 import { PostsComponent } from './posts/posts.component';
 import { MainComponent } from './main.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
     MainRoutingModule,
     NbLayoutModule,
-    NbSidebarModule
+    NbSidebarModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCardModule,
+    NbMenuModule.forRoot(),
+    NbContextMenuModule
   ],
-  providers: [ NbSidebarService ],
+  providers: [ NbSidebarService, NbMenuService ],
   declarations: [ MainComponent , PostsComponent]
 })
 export class MainModule { }
