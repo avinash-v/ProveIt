4f30e9c5388bb80d38fef0f0a0d0fc880367d1ae
kvorhu <akhilsundaram@yahoo.in>
2018-10-17 08:40:23 +0530

    Incomplete stuff

--- Project/Project/resources/endpoints.py
+++ Project/Project/resources/endpoints.py
@@ -3,6 +3,7 @@ from flask_restful import Resource,reqparse,fields,marshal
 from Project.resources.login import Login,Logout
 from Project.resources.signup import SignUp
 from Project.resources.projectBase import ProjectBase, UserProjects
+from Project.resources.UserProfiles import UserProfiles
 #Remove after testing(Not required)
 from Project.resources.reset import EmailVer
 from flask_login import login_required
@@ -16,5 +17,5 @@ api.add_resource(UserProfiles,"/profile/<username>",endpoint="getprofile")
 api.add_resource(UserProfiles,"/profile",endpoint="addProfile")
 api.add_resource(UserProfiles,"/profile/update",endpoint="updateProfile")
 api.add_resource(UserProfiles,"/profile/delete",endpoint="deleteProfile")
-api.add_resource(ProjectBase,'/projects/<string:project_name>',endpoint="getProjectBase") #[GET]- gets project details using projectName, [PUT] - Update Project with the given ProjectName, [POST] -Creates new Project under user
-api.add_resource(UserProjects,'/user/<string:user_name>/projects/',endpoint="getProjectBase2") #[GET] - gets all groups of given user, 
+api.add_resource(ProjectBase,'/projects/<string:project_name>',endpoint="ProjectGroupBase") #[GET]- gets project details using projectName, [PUT] - Update Project with the given ProjectName, [POST] -Creates new Project under user
+api.add_resource(UserProjects,'/user/<string:user_name>/projects/',endpoint="UserProjects") #[GET] - gets all groups of given user, 
