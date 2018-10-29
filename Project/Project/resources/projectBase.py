c70f46cfd125b4bc9005303c7dbb23d6bad2bfe1
kvorhu <akhilsundaram@yahoo.in>
2018-10-29 21:43:17 +0530

    ProjectBase bug and functionality update
    #Fixed some bugs
    #added remove and add members to projects through PUT
    #Added DELETE operation in projects/<project_name> => deletes group if
    done by owner, removes user from group if not owner

--- Project/Project/resources/projectBase.py
+++ Project/Project/resources/projectBase.py
@@ -24,7 +24,7 @@ headers = {
 
 def GetMembers(projectId):
     retVal = []
-    for i in UserToGroup.query.filter_by(groupId=projectId):
+    for i in UserToGroup.query.filter_by(groupId=projectId,relationship="member"):
         mem = User.query.filter_by(id=i.userId).first().username
         retVal.append(mem)
     return retVal
@@ -42,16 +42,92 @@ def GetProjects(userId):
     return retVal1, retVal2
     
 #Please only call this function after validating if Group of grpId exists. Thank you -Akhil
-def AddMembers(members,grpId):
+def ChangeMembers(members,grpId):
     warning = False
     warningMsg = ""
     members = json.loads(members)
-    g = Group.query.filter_by(id=grpId).first() 
-    for i in members:
+    g = Group.query.filter_by(id=grpId).first()
+    g_mem = GetMembers(grpId)
+
+    #lists tracking new members and removals
+    to_add = []
+    to_remove = []
+    
+    #check for additions and removals and append to appropriate list
+    for i in range(len(members)):
+        if members[i] in g_mem:
+            continue
+        else:
+            to_add.append(members[i])
+            continue
+
+    for i in range(len(g_mem)):
+        if g_mem[i] not in members:
+            to_remove.append(g_mem[i])
+            continue
+        else:
+            continue
+
+    #Check and add Users to group
+    warningAdd, warningMsgAdd = AddMembers(to_add,grpId)
+    warningRemove, warningMsgRemove = RemoveMembers(to_remove, grpId)
+    
+    warning = warningAdd or warningRemove
+    warningMsg = warningMsg + warningAdd + warningMsgRemove
+
+    return warning, warningMsg
+
+
+def AddMembers(to_add,grpId):
+    warning = False
+    warningMsg = ""
+    g = Group.query.filter_by(id=grpId).first()
+    for i in to_add:
         u = User.query.filter_by(username=i).first()
         if u is None:
+            warning = True
+            warningMsg = warningMsg + "User "+i+"does not exist;"
             continue
-        if 
+        else:
+            tp = UserToGroup(userId = u.id, groupId = grpId, relationship = "member")
+            try:
+                db.session.add(tp)
+                db.session.commit()
+            except:
+                warning = True
+                warningMsg = warningMsg + "Unknown DB error while trying to add user: "+ i + " to group - refer AddMembers;"
+            continue
+        continue
+    return warning, warningMsg
+
+def RemoveMembers(to_remove,grpId):
+    warning = False
+    warningMsg = ""
+    g = Group.query.filter_by(id=grpId).first()
+    for i in to_add:
+        u = User.query.filter_by(username=i).first()
+        if u is None:
+            warning = True
+            warningMsg = warningMsg + "User "+i+"does not exist while removing member? Insane Error;"
+            continue
+        else:
+            if(i == g.owner):
+                warning = True
+                warningMsg = "Owner cannot remove themselves"
+                continue
+            tp = UserToGroup.query.filter_by(userId = u.id, groupId = grpId, relationship = "member").first()
+            try:
+                db.session.delete(tp)
+                db.session.commit()
+            except:
+                warning = True
+                warningMsg = warningMsg + "Unknown DB error while trying to delete user: "+ i + "from group - refer AddMembers;"
+            continue
+        continue
+    return warning, warningMsg
+
+
+
 
         
 
@@ -154,10 +230,13 @@ class ProjectBase(Resource):
         data = request.get_json(force=True)
         warning = False
         warningMsg = ""
+        isOwner = False
         grp = Group.query.filter_by(name=project_name).first()
         if grp is None:
             return {'description':'Project Does not exist','error':True},200,headers
         if current_user.username == grp.owner:
+            isOwner = True
+        if current_user.username in GetMembers(grp.id):
             try:
                 name = data['name']
                 n = Group.query.filter_by(name = name).first()
@@ -176,14 +255,22 @@ class ProjectBase(Resource):
             except:
                 domain = grp.domain
             try:
-                subowner = data['subowner']
-                s = User.query.filter_by(username = subowner).first()
-                if s is None:
-                    subowner = grp.subOwner
-                    warning = True
-                    warningMsg = warningMsg + "Invalid Subowner;"
+                if(isOwner):
+                    subowner = data['subowner']
+                    s = User.query.filter_by(username = subowner).first()
+                    if s is None:
+                        subowner = grp.subOwner
+                        warning = True
+                        warningMsg = warningMsg + "Invalid Subowner;"
             except:
                 subowner = grp.subOwner
+            try:
+                if(isOwner):
+                    members = data['members']
+                    #HERE
+                    ChangeMembers(members,grpId)  
+            except:
+                members = grp.members
             if(grp.groupType == "researchgroup"):
                 grp.name = name
                 grp.abstract = abstract
@@ -200,11 +287,10 @@ class ProjectBase(Resource):
                     topic = grp.topic
                 try:
                     rg = data['researchGroup']
-                    r = Group.query.filter_by(id = rg).first()
+                    r = Group.query.filter_by(name = rg).first()
                     if r is None:
-                        subowner = grp.subOwner
                         warning = True
-                        warningMsg = warningMsg + "Invalid Subowner;"
+                        warningMsg = warningMsg + "Invalid Research Group;"
                 except:
                     rg = grp.researchGroup
                 grp.topic = topic
@@ -220,7 +306,37 @@ class ProjectBase(Resource):
 
 
         else:
-            return {'description':'Only Project owner can modify Group details','error':True,'warning':warning,'warningMsg':warningMsg},401,headers
+            return {'description':'Only Project members can modify Group details','error':True,'warning':warning,'warningMsg':warningMsg},401,headers
+
+    def delete(self,project_name):
+        warning = False
+        warningMsg = ""
+        grp = Group.query.filter_by(name=project_name).first()
+        if grp is None:
+            return {'description':'Project Does not exist','error':True},200,headers
+        if current_user.username == grp.owner:
+            try:
+                session.delete(grp)
+                session.commit()
+                return {'description':'Successful deletion of Group','error':False,'warning':warning,'warningMsg':warningMsg},201,headers
+            except:
+                warning = True
+                warningMsg = "Db deletin error"
+                return {'description':'Unknown Error, failure to commit','error':True,'warning':warning,'warningMsg':warningMsg},200,headers
+
+        else:
+            if current_user.username in getMembers(grp.id):
+                temp_list = []
+                temp_list.append(current_user.username)
+                warning, warningMsg = RemoveMembers(temp_list,grp.id)
+                if(warning == True):
+                    return {'description':'Failure to delete user from group','error':True,'warning':warning,'warningMsg':warningMsg},200,headers
+                else:
+                    return {'description':'Successful deletion of User from Group','error':False,'warning':warning,'warningMsg':warningMsg},201,headers
+            else:
+                return {'description':'Only User related to Group can Delete Group details','error':True},401,headers
+
+             
 
     
 
