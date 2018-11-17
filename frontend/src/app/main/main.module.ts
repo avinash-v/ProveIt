06908082772694711fd93c00878d704d86ad7014
Arun John <arunjoh@gmail.com>
2018-11-17 04:05:48 +0000

    sanity checks

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -20,21 +20,11 @@ import { AngularFileUploaderModule } from "angular-file-uploader";
 import { MakeProjectComponent } from './make-project/make-project.component';
 import { MakeGroupComponent } from './make-group/make-group.component';
 import { GroupsComponent } from './groups/groups.component';
-<<<<<<< HEAD
 import { RecommendationComponent } from './recommendation/recommendation.component';
 import { ReimbursementsComponent } from './reimbursements/reimbursements.component';
-=======
-
-import { RecommendationComponent } from './recommendation/recommendation.component';
-
->>>>>>> 7ba92c319c30fd574750aaebe9a9b295a8c84cce
 import { GetImageService } from './get-image.service';
 import { FormsModule } from '@angular/forms';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7ba92c319c30fd574750aaebe9a9b295a8c84cce
 
 @NgModule({
   imports: [
@@ -60,11 +50,6 @@ import { FormsModule } from '@angular/forms';
     NbToastrModule.forRoot()
   ],
   providers: [ NbSidebarService, NbMenuService, GetPostService , GetImageService],
-<<<<<<< HEAD
   declarations: [ MainComponent , PostsComponent, PostComponent, HomeComponent, AboutusComponent, MakeProjectComponent, ReimbursementsComponent, RecommendationComponent,MakeGroupComponent, GroupsComponent]
-=======
-  declarations: [ MainComponent , PostsComponent, PostComponent, HomeComponent, AboutusComponent, MakeProjectComponent, MakeGroupComponent, GroupsComponent , RecommendationComponent]
-
->>>>>>> 7ba92c319c30fd574750aaebe9a9b295a8c84cce
 })
 export class MainModule { }
